94fa87fbf46f4f10798ce327feb07fa7f63599ec
lcsmuller <muller.lucas@Hotmail.com>
2022-01-29 17:27:11 -0300

    feat: escape strings pushed with jsonb_push_string()

--- json-build.h
+++ json-build.h
@@ -35,14 +35,13 @@ enum jsonb_state {
     JSONB_OBJECT_NEXT_KEY_OR_CLOSE,
     JSONB_ARRAY_VALUE_OR_CLOSE,
     JSONB_ARRAY_NEXT_VALUE_OR_CLOSE,
-    JSONB_ERROR,
-    JSONB_DONE
+    JSONB_ERROR
 };
 
 /** @brief Handle for building a JSON string */
 typedef struct jsonb {
     /** expected next input */
-    enum jsonb_state st_stack[JSONB_MAX_DEPTH];
+    enum jsonb_state st_stack[JSONB_MAX_DEPTH + 1];
     /** pointer to stack top */
     enum jsonb_state *st_top;
     /** offset in the JSON buffer (current length) */
@@ -208,8 +207,6 @@ _jsonb_eval_state(enum jsonb_state state)
         return "array value or close";
     case JSONB_ARRAY_NEXT_VALUE_OR_CLOSE:
         return "array next value or close";
-    case JSONB_DONE:
-        return "done";
     case JSONB_ERROR:
         return "error";
     default:
@@ -219,7 +216,7 @@ _jsonb_eval_state(enum jsonb_state state)
 #define TRACE(prev, next)                                                     \
     do {                                                                      \
         enum jsonb_state _prev = prev, _next = next;                          \
-        fprintf(stderr, "%s():L%d|%s -> %s\n", __func__, __LINE__,            \
+        fprintf(stderr, "%s():L%d | %s -> %s\n", __func__, __LINE__,          \
                 _jsonb_eval_state(_prev), _jsonb_eval_state(_next));          \
     } while (0)
 #define DECORATOR(d) d
@@ -253,8 +250,6 @@ jsonb_init(jsonb *builder)
     static jsonb empty_builder;
     *builder = empty_builder;
     builder->st_top = builder->st_stack;
-    STACK_HEAD(builder, JSONB_DONE);
-    STACK_PUSH(builder, JSONB_ARRAY_OR_OBJECT_OR_VALUE);
 }
 
 long
@@ -402,12 +397,8 @@ jsonb_push_token(
 long
 jsonb_push_bool(jsonb *builder, int boolean, char buf[], size_t bufsize)
 {
-    long ret;
-    if (boolean)
-        ret = jsonb_push_token(builder, "true", 4, buf, bufsize);
-    else
-        ret = jsonb_push_token(builder, "false", 5, buf, bufsize);
-    return ret;
+    if (boolean) return jsonb_push_token(builder, "true", 4, buf, bufsize);
+    return jsonb_push_token(builder, "false", 5, buf, bufsize);
 }
 
 long
@@ -416,15 +407,99 @@ jsonb_push_null(jsonb *builder, char buf[], size_t bufsize)
     return jsonb_push_token(builder, "null", 4, buf, bufsize);
 }
 
+static long
+_jsonb_escape(size_t pos, char str[], size_t len, char buf[], size_t bufsize)
+{
+    char *esc_tok = NULL, _esc_tok[8] = "\\u00";
+    char *esc_buf = NULL;
+    int extra_bytes = 0;
+    size_t i;
+
+/* 1st iteration, esc_buf is NULL and count extra_bytes needed for escaping
+ * 2st iteration, esc_buf is not NULL, and does escaping.  */
+second_iter:
+    for (i = 0; i < len; ++i) {
+        unsigned char c = str[i];
+        esc_tok = NULL;
+        switch (c) {
+        case 0x22:
+            esc_tok = "\\\"";
+            break;
+        case 0x5C:
+            esc_tok = "\\\\";
+            break;
+        case '\b':
+            esc_tok = "\\b";
+            break;
+        case '\f':
+            esc_tok = "\\f";
+            break;
+        case '\n':
+            esc_tok = "\\n";
+            break;
+        case '\r':
+            esc_tok = "\\r";
+            break;
+        case '\t':
+            esc_tok = "\\t";
+            break;
+        default:
+            if (c <= 0x1F) {
+                static const char tohex[] = "0123456789abcdef";
+                _esc_tok[4] = tohex[c >> 4];
+                _esc_tok[5] = tohex[c & 0xF];
+                _esc_tok[6] = 0;
+                esc_tok = _esc_tok;
+            }
+        }
+
+        if (esc_tok) {
+            int j;
+            for (j = 0; esc_tok[j]; j++) {
+                if (!esc_buf) /* count how many extra bytes are needed */
+                    continue;
+                *esc_buf = esc_tok[j];
+                ++esc_buf;
+            }
+            extra_bytes += j - 1;
+        }
+        else if (esc_buf) {
+            *esc_buf = c;
+            ++esc_buf;
+        }
+    }
+
+    if (pos + len + extra_bytes > bufsize) {
+        return JSONB_ERROR_NOMEM;
+    }
+    if (esc_buf) {
+        return len + extra_bytes;
+    }
+    if (!extra_bytes) {
+        size_t j;
+        for (j = 0; j < len; ++j)
+            buf[pos + j] = str[j];
+        return len;
+    }
+
+    esc_buf = buf + pos;
+    extra_bytes = 0;
+
+    goto second_iter;
+}
+
 long
 jsonb_push_string(
     jsonb *builder, char str[], size_t len, char buf[], size_t bufsize)
 {
     size_t pos = 0;
+    long ret;
     switch (*builder->st_top) {
     case JSONB_ARRAY_OR_OBJECT_OR_VALUE:
         BUFFER_COPY_CHAR(builder, '"', pos, buf, bufsize);
-        BUFFER_COPY(builder, str, len, pos, buf, bufsize);
+        ret = _jsonb_escape(pos + builder->pos, str, len, buf, bufsize);
+        if (ret < 0) return ret;
+        pos += ret;
         BUFFER_COPY_CHAR(builder, '"', pos, buf, bufsize);
         STACK_POP(builder);
         break;
@@ -433,13 +508,17 @@ jsonb_push_string(
         /* fall-through */
     case JSONB_ARRAY_VALUE_OR_CLOSE:
         BUFFER_COPY_CHAR(builder, '"', pos, buf, bufsize);
-        BUFFER_COPY(builder, str, len, pos, buf, bufsize);
+        ret = _jsonb_escape(pos + builder->pos, str, len, buf, bufsize);
+        if (ret < 0) return ret;
+        pos += ret;
         BUFFER_COPY_CHAR(builder, '"', pos, buf, bufsize);
         STACK_HEAD(builder, JSONB_ARRAY_NEXT_VALUE_OR_CLOSE);
         break;
     case JSONB_OBJECT_VALUE:
         BUFFER_COPY_CHAR(builder, '"', pos, buf, bufsize);
-        BUFFER_COPY(builder, str, len, pos, buf, bufsize);
+        ret = _jsonb_escape(pos + builder->pos, str, len, buf, bufsize);
+        if (ret < 0) return ret;
+        pos += ret;
         BUFFER_COPY_CHAR(builder, '"', pos, buf, bufsize);
         STACK_HEAD(builder, JSONB_OBJECT_NEXT_KEY_OR_CLOSE);
         break;
